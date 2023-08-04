31793c7ab5d04716b95444878bc28ebd1e29f798
habara keigo <habara.keigo@linecorp.com>
2023-08-04 13:36:15 +0900

    fix viewport

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import "./Game";
 
 function App() {
-  return <div id="phaser-container" className="App"></div>;
+  return (
+    <div id="phaser-container" className="App" width="100vw" height="100vh">
+    </div>
+  );
 }
 
 export default App;
